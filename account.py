ac6edb4a1d4cf1481428b69acec9c3bf82fa2807
pybites <pybites@projects.bobbelderbos.com>
2017-06-15 13:22:42 +0200

    init commit

---
+++ account.py
@@ -0,0 +1,71 @@
+from functools import total_ordering
+
+
+@total_ordering
+class Account:
+    'A simple account class'
+
+    def __init__(self, owner, amount=0):
+        'This is the constructor that lets us create objects from this class'
+        self.owner = owner
+        self.amount = amount
+        self._transactions = []
+
+    def __repr__(self):
+        return 'Account({!r}, {!r})'.format(self.owner, self.amount)
+
+    def __str__(self):
+        return 'Account of {} with balance: {}'.format(self.owner, self.amount)
+
+    def add_transaction(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError('please use int for amount')
+        self._transactions.append(amount)
+
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, position):
+        return self._transactions[position]
+
+    def __reversed__(self):
+        return sorted(self, reverse=True)
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __add__(self, other):
+        owner = '{}&{}'.format(self.owner, other.owner)
+        start_amount = self.amount + other.amount
+        acc = Account(owner, start_amount)
+        for t in list(self) + list(other):
+            acc.add_transaction(t)
+        return acc
+
+    def __call__(self):
+        print('Start amount: {}'.format(self.amount))
+        print('Transactions: ')
+        for transaction in self:
+            print(transaction)
+        print('\nBalance: {}'.format(self.balance))
+
+    def __enter__(self):
+        print('ENTER WITH: making backup of transactions for rollback')
+        self._copy_transactions = list(self._transactions)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('EXIT WITH:', end=' ')
+        if exc_type:
+            self._transactions = self._copy_transactions
+            print('rolling back to previous transactions')
+            print('transaction resulted in {} ({})'.format(exc_type.__name__, exc_val))  # noqa E501
+        else:
+            print('transaction ok')
